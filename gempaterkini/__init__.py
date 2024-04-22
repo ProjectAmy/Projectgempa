e050abbab7d6135f12fef77e130a790ac94855e0
amysidra <amysidra@gmail.com>
2024-04-22 20:55:33 +0700

    gempa

--- gempaterkini/__init__.py
+++ gempaterkini/__init__.py
@@ -13,24 +13,31 @@ def ekstrasi_data():
     Dirasakan (Skala MMI): III Mamasa
     :return:
     """
+    try:
+        content = requests.get('https://bmkg.go.id/', headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"})
+    except Exception:
+        return None
+    if content.status_code == 200:
+        print(content.text)
+        # soup = BeautifulSoup(content)
+        # print(soup.prettify())
 
-    content = requests.get('https://www.bmkg.go.id/')
-    print(content.status_code)
-    # soup = BeautifulSoup(content)
-    # print(soup.prettify())
-
-    hasil = dict()
-    hasil['tanggal'] = '19 April 2024'
-    hasil['waktu'] = '14:22:55 WIB'
-    hasil['magnitudo'] = 3.5
-    hasil['kedalaman'] = '6 km'
-    hasil['lokasi'] = '2.93 LS - 119.40 BT'
-    hasil['pusat'] = 'berada di darat 8 km Tenggara Mamasa'
-    hasil['dirasakan'] = 'III Mamasa'
-    return hasil
-
+        hasil = dict()
+        hasil['tanggal'] = '19 April 2024'
+        hasil['waktu'] = '14:22:55 WIB'
+        hasil['magnitudo'] = 3.5
+        hasil['kedalaman'] = '6 km'
+        hasil['lokasi'] = '2.93 LS - 119.40 BT'
+        hasil['pusat'] = 'berada di darat 8 km Tenggara Mamasa'
+        hasil['dirasakan'] = 'III Mamasa'
+        return hasil
+    else:
+        return None
 
 def tampilkan_data(result):
+    if result is None:
+        print('tidak bisa menemukan data yang dimaksud')
+        return
     print('\nGempa terakhir berdasarkan BMKG')
     print(f"Tanggal : {result['tanggal']}")
     print(f"Waktu : {result['waktu']}")
