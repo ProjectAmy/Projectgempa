a231deb8290fb428570fe91756c012c3a06146bb
amysidra <amysidra@gmail.com>
2024-04-22 19:39:02 +0700

    gempa

--- main.py
+++ main.py
@@ -1,40 +1,9 @@
 """
 ini adalah project pembuatan aplikasi gempa
 """
-
-def ekstrasi_data():
-    """
-    Tanggal : 19 April 2024
-    Waktu : 14:22:55 WIB
-    Magnituoda : 3.5
-    Kedalaman : 6 km
-    Lokasi : 2.93 LS - 119.40 BT
-    Pusat gempa : berada di darat 8 km Tenggara Mamasa
-    Dirasakan (Skala MMI): III Mamasa
-    :return:
-    """
-    hasil = dict()
-    hasil['tanggal'] = '19 April 2024'
-    hasil['waktu'] = '14:22:55 WIB'
-    hasil['magnitudo'] = 3.5
-    hasil['kedalaman'] = '6 km'
-    hasil['lokasi'] = '2.93 LS - 119.40 BT'
-    hasil['pusat'] = 'berada di darat 8 km Tenggara Mamasa'
-    hasil['dirasakan'] = 'III Mamasa'
-    return hasil
-
-
-def tampilkan_data(result):
-    print('\nGempa terakhir berdasarkan BMKG')
-    print(f"Tanggal : {result['tanggal']}")
-    print(f"Waktu : {result['waktu']}")
-    print(f"Magnitudo : {result['magnitudo']}")
-    print(f"Kedalaman : {result['kedalaman']}")
-    print(f"Lokasi : {result['lokasi']}")
-    print(f"Pusat Gempa : {result['pusat']}")
-    print(f"Dirasakan : {result['dirasakan']}")
+import gempaterkini
 
 if __name__ == '__main__':
     print("aplikasi utama")
-    result = ekstrasi_data()
-    tampilkan_data(result)
+    result = gempaterkini.ekstrasi_data()
+    gempaterkini.tampilkan_data(result)
