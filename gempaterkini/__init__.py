c48c8f9d78a995220ae4b9e12c4439394115621c
amysidra <amysidra@gmail.com>
2024-04-23 08:34:39 +0700

    selesai

--- gempaterkini/__init__.py
+++ gempaterkini/__init__.py
@@ -21,19 +21,43 @@ def ekstrasi_data():
     if content.status_code == 200:
         soup = BeautifulSoup(content.text, 'html.parser')
         tnw = soup.find('span', {'class':'waktu'})
-        tnw = tnw.text.split(', ')
+        tnw = tnw.text.split(', ') # membuat array dengan pemisah
 
         tanggal = tnw[0]
         waktu = tnw[1]
 
+        ket = soup.find('div', {'class':'col-md-6 col-xs-6 gempabumi-detail no-padding'})
+        ket = ket.findChildren('li') # mencari children / turunan
+
+        # variable standar
+        i = 0
+        magnitudo = None
+        kedalaman = None
+        koordinat = None
+        lokasi = None
+        skala = None
+
+        for res in ket: #menampilkan semua children dalam bentuk list
+            if i == 1:
+                magnitudo = res.text
+            elif i == 2:
+                kedalaman = res.text
+            elif i == 3:
+                koordinat = res.text
+            elif i == 4:
+                lokasi = res.text
+            elif i == 5:
+                skala = res.text
+            i = i + 1
+
         hasil = dict()
         hasil['tanggal'] = tanggal
         hasil['waktu'] = waktu
-        hasil['magnitudo'] = 3.5
-        hasil['kedalaman'] = '6 km'
-        hasil['lokasi'] = '2.93 LS - 119.40 BT'
-        hasil['pusat'] = 'berada di darat 8 km Tenggara Mamasa'
-        hasil['dirasakan'] = 'III Mamasa'
+        hasil['magnitudo'] = magnitudo
+        hasil['kedalaman'] = kedalaman
+        hasil['koordinat'] = koordinat
+        hasil['lokasi'] = lokasi
+        hasil['skala'] = skala
         return hasil
     else:
         return None
@@ -42,14 +66,14 @@ def tampilkan_data(result):
     if result is None:
         print('tidak bisa menemukan data yang dimaksud')
         return
-    print('\nGempa terakhir berdasarkan BMKG')
+    print('\nGempa terakhir berdasarkan BMKG\n')
     print(f"Tanggal : {result['tanggal']}")
     print(f"Waktu : {result['waktu']}")
     print(f"Magnitudo : {result['magnitudo']}")
     print(f"Kedalaman : {result['kedalaman']}")
+    print(f"Koordinat : {result['koordinat']}")
     print(f"Lokasi : {result['lokasi']}")
-    print(f"Pusat Gempa : {result['pusat']}")
-    print(f"Dirasakan : {result['dirasakan']}")
+    print(f"Skala : {result['skala']}")
 
 # if __name__ == "__main__" :
 #     print('ini adalah package')
