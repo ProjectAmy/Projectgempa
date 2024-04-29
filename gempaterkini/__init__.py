bdf3893848bb5c47da7d87e1bf5f4a73a05d88fa
amysidra <amysidra@gmail.com>
2024-04-29 12:26:50 +0700

    gempa

--- gempaterkini/__init__.py
+++ gempaterkini/__init__.py
@@ -1,79 +1,99 @@
 import requests
 from bs4 import BeautifulSoup
 
+class bencana:
 
-def ekstrasi_data():
-    """
-    Tanggal : 19 April 2024
-    Waktu : 14:22:55 WIB
-    Magnituoda : 3.5
-    Kedalaman : 6 km
-    Lokasi : 2.93 LS - 119.40 BT
-    Pusat gempa : berada di darat 8 km Tenggara Mamasa
-    Dirasakan (Skala MMI): III Mamasa
-    :return:
-    """
-    try:
-        content = requests.get('https://bmkg.go.id/', headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"})
-    except Exception:
-        return None
+    def __init__(self, url, description):
+    # init adalah tempat untuk menampung attribute (variable) yang dibutuhkan class
+        self.description = description
+        self.result = None
+        self.url = url
 
-    if content.status_code == 200:
-        soup = BeautifulSoup(content.text, 'html.parser')
-        tnw = soup.find('span', {'class':'waktu'})
-        tnw = tnw.text.split(', ') # membuat array dengan pemisah
+    def ekstrasi_data(self):
+        pass
 
-        tanggal = tnw[0]
-        waktu = tnw[1]
+    def tampilkan_data(self):
+        pass
 
-        ket = soup.find('div', {'class':'col-md-6 col-xs-6 gempabumi-detail no-padding'})
-        ket = ket.findChildren('li') # mencari children / turunan
+    def run(self):
+        self.ekstrasi_data()
+        self.tampilkan_data()
 
-        # variable standar
-        i = 0
-        magnitudo = None
-        kedalaman = None
-        koordinat = None
-        lokasi = None
-        skala = None
+class infobanjir(bencana):
+    def __init__(self, url):
+        super(infobanjir, self).__init__(url, "ini adalah info terakhir banjir terkini")
 
-        for res in ket: #menampilkan semua children dalam bentuk list
-            if i == 1:
-                magnitudo = res.text
-            elif i == 2:
-                kedalaman = res.text
-            elif i == 3:
-                koordinat = res.text
-            elif i == 4:
-                lokasi = res.text
-            elif i == 5:
-                skala = res.text
-            i = i + 1
+class infogempa(bencana): # Parent nya adalah bencana
+    def __init__(self, url):
+        super(infogempa, self).__init__(url, "ini adalah info terakhir gempa terkini")
 
-        hasil = dict()
-        hasil['tanggal'] = tanggal
-        hasil['waktu'] = waktu
-        hasil['magnitudo'] = magnitudo
-        hasil['kedalaman'] = kedalaman
-        hasil['koordinat'] = koordinat
-        hasil['lokasi'] = lokasi
-        hasil['skala'] = skala
-        return hasil
-    else:
-        return None
+    def ekstrasi_data(self): # Semua method harus ada atribute self
 
-def tampilkan_data(result):
-    if result is None:
-        print('tidak bisa menemukan data yang dimaksud')
-        return
-    print('\nGempa terakhir berdasarkan BMKG\n')
-    print(f"Tanggal : {result['tanggal']}")
-    print(f"Waktu : {result['waktu']}")
-    print(f"Magnitudo : {result['magnitudo']}")
-    print(f"Kedalaman : {result['kedalaman']}")
-    print(f"Koordinat : {result['koordinat']}")
-    print(f"Lokasi : {result['lokasi']}")
-    print(f"Skala : {result['skala']}")
+        try:
+            content = requests.get(self.url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"})
+        except Exception:
+            return None
+
+        if content.status_code == 200:
+            soup = BeautifulSoup(content.text, 'html.parser')
+            tnw = soup.find('span', {'class':'waktu'})
+            tnw = tnw.text.split(', ') # membuat array dengan pemisah
+
+            tanggal = tnw[0]
+            waktu = tnw[1]
+
+            ket = soup.find('div', {'class':'col-md-6 col-xs-6 gempabumi-detail no-padding'})
+            ket = ket.findChildren('li') # mencari children / turunan
+
+            # variable standar
+            i = 0
+            magnitudo = None
+            kedalaman = None
+            koordinat = None
+            lokasi = None
+            skala = None
+
+            for res in ket: # menampilkan semua children dalam bentuk list
+                if i == 1:
+                    magnitudo = res.text
+                elif i == 2:
+                    kedalaman = res.text
+                elif i == 3:
+                    koordinat = res.text
+                elif i == 4:
+                    lokasi = res.text
+                elif i == 5:
+                    skala = res.text
+                i = i + 1
+
+            hasil = dict()
+            hasil['tanggal'] = tanggal
+            hasil['waktu'] = waktu
+            hasil['magnitudo'] = magnitudo
+            hasil['kedalaman'] = kedalaman
+            hasil['koordinat'] = koordinat
+            hasil['lokasi'] = lokasi
+            hasil['skala'] = skala
+            self.result =  hasil
+        else:
+            return None
+
+    def tampilkan_data(self):
+        if self.result is None:
+            print('tidak bisa menemukan data yang dimaksud')
+            return
+        print('\nIni adalah aplikasi untuk update info bencana terakhir\n')
+        print(f"Tanggal : {self.result['tanggal']}")
+        print(f"Waktu : {self.result['waktu']}")
+        print(f"Magnitudo : {self.result['magnitudo']}")
+        print(f"Kedalaman : {self.result['kedalaman']}")
+        print(f"Koordinat : {self.result['koordinat']}")
+        print(f"Lokasi : {self.result['lokasi']}")
+        print(f"Skala : {self.result['skala']}")
+
+    # def run(self):
+    #     self.ekstrasi_data()
+    #     self.tampilkan_data()
 
 # if __name__ == "__main__" :
 #     print('ini adalah package')
--- main.py
+++ main.py
@@ -4,6 +4,11 @@ ini adalah project pembuatan aplikasi gempa
 import gempaterkini
 
 if __name__ == '__main__':
-    print("aplikasi utama")
-    result = gempaterkini.ekstrasi_data()
-    gempaterkini.tampilkan_data(result)
+
+    gempaindo = gempaterkini.infogempa('http://bmkg.go.id')
+    print("Deskripsi :", gempaindo.description)
+    gempaindo.run()
+
+    banjirindo = gempaterkini.infobanjir('http://bmkg.go.id')
+    print("Deskripsi :", banjirindo.description)
+    banjirindo.run()
